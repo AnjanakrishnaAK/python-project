2054e6158132faba689f22bab3886f304d5cc57f
AnjanakrishnaAK <109781826+AnjanakrishnaAK@users.noreply.github.com>
2026-01-30 15:56:40 +0530

    Add files via upload

--- Zecpath model/Zecpath/Zecpath/settings/base.py
+++ Zecpath model/Zecpath/Zecpath/settings/base.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-"""
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 load_dotenv(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
@@ -129,4 +128,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-"""

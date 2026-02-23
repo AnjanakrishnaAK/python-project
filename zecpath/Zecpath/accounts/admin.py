da1092d1f1a722be7b4fb8d3d798afc7b6cb4119
AnjanakrishnaAK <109781826+AnjanakrishnaAK@users.noreply.github.com>
2026-02-23 11:30:35 +0530

    Delete zecpath/Zecpath directory

--- zecpath/Zecpath/accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User,CandidateProfile, EmployerProfile
-
-admin.site.register(User)
-admin.site.register(CandidateProfile)
-admin.site.register(EmployerProfile)

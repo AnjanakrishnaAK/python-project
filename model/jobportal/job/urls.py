ce7d98d60930ec2e8254096a6cee50dc0edcf95b
AnjanakrishnaAK <109781826+AnjanakrishnaAK@users.noreply.github.com>
2026-02-23 15:24:22 +0530

    Delete model/jobportal directory

--- model/jobportal/job/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, users_list, jobs_list, applications_list
-
-urlpatterns = [
-    path('', home),
-    path('users/', users_list),
-    path('jobs/', jobs_list),
-    path('applications/', applications_list),
-]
